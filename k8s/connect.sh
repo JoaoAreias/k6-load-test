97c4278c984f7201741b22971c87d5a367d6fc16
Joao Areias <joaoareiasmoraes@gmail.com>
2025-11-17 17:32:22 -0300

    feat/add script to connect with cluster

---
+++ k8s/connect.sh
@@ -0,0 +1,8 @@
+#  ------------ Connect ------------
+#  This script establishes a secure tunnel to the Minikube cluster,
+#  binding it to the localhost address and forwarding Graphana's port.
+
+minikube tunnel --bind-address="127.0.0.1" -c &
+
+export POD_NAME=$(kubectl --namespace default get pod -l "app.kubernetes.io/name=grafana,app.kubernetes.io/instance=prometheus" -oname)
+kubectl --namespace default port-forward $POD_NAME 3000:3000
